39d4e8086833905894ce3f4f746736cc61e38ea5
igurmansingh <S.gurman28@23>
2020-07-23 18:42:08 -0400

    Initial Commit

---
+++ RegistrationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegistrationApp
+//
+//  Created by Gurman Singh on 2020-07-23.
+//  Copyright © 2020 Group6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
